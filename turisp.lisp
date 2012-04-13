9b85a4306d2d2d70cdb25842c4e202888c5f8824
FMdKdD <fmdkdd@gmail.com>
2012-04-13 19:08:29 +0200

    Add GLP3 license notice

--- turisp.lisp
+++ turisp.lisp
@@ -1,3 +1,19 @@
+;; Turing machine simulator in Common Lisp.
+;; Copyright (C) 2011 fmdkdd <fmdkdd@gmail.com>
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ;; Tape functions.
 
@@ -5,33 +21,33 @@
 ;; its origin.
 (defun make-tape (contents)
   (make-array (length contents) :initial-contents contents
-              :adjustable t :fill-pointer t))
+				  :adjustable t :fill-pointer t))
 
 ;; Return the symbol of 'tape' at 'index'. If 'index' is outside the
 ;; tape bounds, the machine special blank symbol is returned.
 (defun get-symbol (machine tape index)
   (if (< index (length tape))
-      (elt tape index)
-    (machine-blank machine)))
+		(elt tape index)
+	 (machine-blank machine)))
 
 ;; Set 'x' to be the symbol of 'tape' at 'index', and return
 ;; 'tape'. 'index' should have a value not greater than the tape
 ;; length.
 (defun set-symbol (tape index x)
   (progn
-    (if (< index (length tape))
-        (setf (elt tape index) x)
-      (vector-push-extend x tape))
-    tape))
+	 (if (< index (length tape))
+		  (setf (elt tape index) x)
+		(vector-push-extend x tape))
+	 tape))
 
 ;; Move 'index' according to 'switch' direction (either "L" or "R"),
 ;; and return the new index.
 (defun move-head (index switch)
   (if (equal "L" switch)
-      (if (= 0 index)
-          "Error - can't go left of origin."
+		(if (= 0 index)
+			 "Error - can't go left of origin."
 		  (- index 1))
-    (+ index 1)))
+	 (+ index 1)))
 
 ;;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ;; Rules functions.
@@ -89,27 +105,27 @@
 ;; state and symbol.
 (defun find-rule (machine state symbol)
   (find (list state symbol) (machine-rules machine)
-        :test 'equal
-        :key (lambda (rule) (list (rule-from-state rule) (rule-from-symbol rule)))))
+		  :test 'equal
+		  :key (lambda (rule) (list (rule-from-state rule) (rule-from-symbol rule)))))
 
 ;; Whether 'state' is a final state of 'machine'.
 (defun is-final-state? (machine state)
   (not (null (find state (machine-final-states machine)
-                   :test 'equal))))
+						 :test 'equal))))
 
 ;; The simulator function. Takes a machine and a tape as arguments.
 (defun run-machine (machine tape
-                            &optional (state (machine-initial-state machine))
-                            (tape-head 0))
+									 &optional (state (machine-initial-state machine))
+									 (tape-head 0))
   (let* ((symbol (get-symbol machine tape tape-head))
-         (rule (find-rule machine state symbol)))
-    (when (is-final-state? machine state)
-      (print "Calcul acceptant"))
-    (if (null rule) (pretty-print tape (machine-blank machine))
-      (run-machine machine
-                   (set-symbol tape tape-head (rule-next-symbol rule))
-                   (rule-next-state rule)
-                   (move-head tape-head (rule-switch rule))))))
+			(rule (find-rule machine state symbol)))
+	 (when (is-final-state? machine state)
+		(print "Calcul acceptant"))
+	 (if (null rule) (pretty-print tape (machine-blank machine))
+		(run-machine machine
+						 (set-symbol tape tape-head (rule-next-symbol rule))
+						 (rule-next-state rule)
+						 (move-head tape-head (rule-switch rule))))))
 
 ;;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ;; Main course !
